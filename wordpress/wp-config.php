c49c5f0015a43decc108239651e0f19b9059335b
Mohamed Arbi <mohamedarbi458@gmail.com>
2021-07-09 01:57:35 +0100

    Add existing file

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -22,16 +22,16 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'wp_bd' );
+define( 'DB_NAME', 'sql11423847' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'sql11423847' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Dh4YKrqnxm' );
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sql11.freesqldatabase.com' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
